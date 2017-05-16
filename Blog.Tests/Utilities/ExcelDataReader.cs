4f71041048a21d83205d748f0a81cbd3395e2443
Filip Filipov <filiprf.softuni@gmail.com>
2017-05-16 22:16:55 +0300

    Add test project and a generic ExcelDataReader.

---
+++ Blog.Tests/Utilities/ExcelDataReader.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using Dapper;
+using NUnit.Framework;
+
+namespace Blog.Tests.Utilities
+{
+    public class ExcelDataReader
+    {
+        public static string TestDataFileConnection()
+        {
+            var path = Path.GetFullPath(Path.Combine(
+                TestContext.CurrentContext.TestDirectory,
+                ConfigurationManager.AppSettings["TestDataPath"]));
+            return $"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = { path }; Extended Properties=Excel 12.0;";
+        }
+
+        public static T GetTestData<T>()
+        {
+            using (var connection = new OleDbConnection(TestDataFileConnection()))
+            {
+                var sheetName = typeof(T).Name;
+                var keyName = TestContext.CurrentContext.Test.Name;
+
+                connection.Open();
+                var query = $"SELECT * FROM [{sheetName}$] WHERE Key = '{keyName}'";
+
+                return connection.Query<T>(query).Single();
+            }
+        }
+    }
+}
