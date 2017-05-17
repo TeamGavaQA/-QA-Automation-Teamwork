dd2eb55635370867ed4c0ef95284c7c59249a888
Filip Filipov <filiprf.softuni@gmail.com>
2017-05-17 21:29:23 +0300

    Add AbstractPage to project with some common methods to use as a base.

---
+++ Blog.Tests/Pages/AbstractPage/AbstarctPage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace Blog.Tests.Pages.AbstractPage
+{
+    public abstract partial class AbstarctPage
+    {
+        protected string baseUrl = ConfigurationManager.AppSettings["URL"];
+
+        protected AbstarctPage(IWebDriver driver)
+        {
+            this.Driver = driver;
+            this.Wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(10));
+            this.Builder = new Actions(this.Driver);
+
+        }
+
+        protected virtual string URL { get; set; }
+
+        protected IWebDriver Driver { get; }
+
+        protected WebDriverWait Wait { get; }
+
+        protected Actions Builder { get; set; }
+
+        protected void NavigateTo()
+        {
+            this.Driver.Navigate().GoToUrl(URL);
+        }
+
+        protected void FollowLink(string linkText)
+        {
+            this.Wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText(linkText))).Click();
+        }
+
+        protected static void Type(IWebElement element, string text)
+        {
+            element.Clear();
+            element.SendKeys(text);
+        }
+
+        protected static void ClickOnElements(
+            IReadOnlyList<IWebElement> elements, IReadOnlyList<bool> values)
+        {
+            for (var i = 0; i < values.Count; i++)
+            {
+                if (values[i])
+                {
+                    elements[i].Click();
+                }
+            }
+        }
+
+    }
+}
---
+++ Blog.Tests/Pages/AbstractPage/AbstractPageAsserter.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Blog.Tests.Pages.AbstractPage
+{
+    public abstract partial class AbstarctPage
+    {
+        public void AssertTextMatch(IWebElement element, string text)
+        {
+            StringAssert.Contains(text, element.Text);
+        }
+
+        public void AssertHasClass(IWebElement target, string classValue)
+        {
+            StringAssert.Contains(classValue, target.GetAttribute("class"));
+        }
+
+        public void AssertLacksClass(IWebElement target, string classValue)
+        {
+            StringAssert.DoesNotContain(classValue, target.GetAttribute("class"));
+        }
+    }
+}
