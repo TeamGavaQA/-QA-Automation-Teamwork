3c433634da1f06efa30fefb8055d7acffc747ba6
Filip Filipov <filiprf.softuni@gmail.com>
2017-05-21 18:37:20 +0300

    Implement browser hosting much more flexibly.

--- Blog.Tests/Utilities/BrowserHosts.cs
+++ Blog.Tests/Utilities/BrowserHosts.cs
@@ -1,8 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Remote;
 using TestStack.Seleno.Configuration;
 
@@ -10,53 +8,38 @@ namespace Blog.Tests.Utilities
 {
     public static class BrowserHosts
     {
-        private static readonly Func<RemoteWebDriver, SelenoHost> hostInit = driver =>
-        {
-            var host = new SelenoHost();
-            host.Run("Blog", 60634, c => c.WithRemoteWebDriver(() => driver));
-
-            return host;
-        };
-
-        private static readonly Lazy<SelenoHost> _chromeHost = new Lazy<SelenoHost>(() =>
-            hostInit(new ChromeDriver()));
-        private static readonly Lazy<SelenoHost> _firefoxHost = new Lazy<SelenoHost>(() =>
-            hostInit(new FirefoxDriver()));
-        private static readonly Lazy<SelenoHost> _internetExplorerHost = new Lazy<SelenoHost>(() =>
-            hostInit(new InternetExplorerDriver()));
-
-        private static IWebDriver Chrome => _chromeHost.Value.Application.Browser;
-        private static IWebDriver Firefox => _firefoxHost.Value.Application.Browser;
-        private static IWebDriver InternetExplorer => _internetExplorerHost.Value.Application.Browser;
+        private static readonly Dictionary<Type, SelenoHost> _instances =
+            new Dictionary<Type, SelenoHost>();
 
         public static IWebDriver GetInstance<T>() where T : IWebDriver
         {
-            switch (typeof(T).Name)
+            var type = typeof(T);
+            if (!_instances.ContainsKey(type))
             {
-                case nameof(ChromeDriver):
-                    return Chrome;
-                case nameof(FirefoxDriver):
-                    return Firefox;
-                case nameof(InternetExplorerDriver):
-                    return InternetExplorer;
-                default:
-                    throw new ArgumentException("Unsupported browser type", typeof(T).Name);
+                var host = new SelenoHost();
+                Func<RemoteWebDriver> browserConstructor;
+                switch (type.ToString())
+                {
+                    // Override the constructor for specific browsers here if needed.
+                    default:
+                        browserConstructor = () =>
+                            (RemoteWebDriver) Activator.CreateInstance(typeof(T));
+                        break;
+                }
+
+                host.Run("Blog", 60634, c => c.WithRemoteWebDriver(browserConstructor));
+
+                _instances[type] = host;
             }
+
+            return _instances[type].Application.Browser;
         }
 
         public static void TerminateInstances()
         {
-            if (_chromeHost.IsValueCreated)
+            foreach (var selenoHost in _instances.Values)
             {
-                Chrome.Quit();
-            }
-            if (_firefoxHost.IsValueCreated)
-            {
-                Firefox.Quit();
-            }
-            if (_internetExplorerHost.IsValueCreated)
-            {
-                InternetExplorer.Quit();
+                selenoHost.Application.Browser.Quit();
             }
         }
     }
