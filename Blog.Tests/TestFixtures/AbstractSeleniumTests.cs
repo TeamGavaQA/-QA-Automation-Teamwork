ed022f4b8299e835ed47386ed738afcbb782ab27
Filip Filipov <filiprf.softuni@gmail.com>
2017-05-24 17:23:12 +0300

    Take screenshot on test failure.

--- Blog.Tests/TestFixtures/AbstractSeleniumTests.cs
+++ Blog.Tests/TestFixtures/AbstractSeleniumTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
 using Blog.Tests.Pages.AbstractPage;
 using Blog.Tests.Utilities;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
@@ -33,6 +36,25 @@ namespace Blog.Tests.TestFixtures
         {
             this.Page.NavigateTo();
             this.Model = ExcelDataReader.GetTestData<TModel>();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                var screenshotPath = Path.GetFullPath(Path.Combine(
+                    TestContext.CurrentContext.TestDirectory,
+                    ConfigurationManager.AppSettings["FailedTestScreenshotsPath"]));
+                if (!Directory.Exists(screenshotPath))
+                {
+                    Directory.CreateDirectory(screenshotPath);
+                }
+
+                ((ITakesScreenshot) this.Driver).GetScreenshot().SaveAsFile(
+                    Path.Combine(screenshotPath, $"{TestContext.CurrentContext.Test.Name}.jpg"),
+                    ScreenshotImageFormat.Jpeg);
+            }
         }
     }
 
